e0e4b42ffba32f3f37d3cf7bbdc616d4dd8c38fa
Martin White <11601920+mjwhite8119@users.noreply.github.com>
2024-02-26 14:39:53 -0800

    Added RomiSubsystem

---
+++ RomiSubsystem/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,169 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.util.sendable.SendableRegistry;
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.romi.RomiGyro;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Drivetrain extends SubsystemBase {
+
+  // The Romi has the left and right motors set to
+  // PWM channels 0 and 1 respectively
+  private final Spark leftMotor = new Spark(0);
+  private final Spark rightMotor = new Spark(1);
+
+  // The Romi has onboard encoders that are hardcoded
+  // to use DIO pins 4/5 and 6/7 for the left and right
+  private final Encoder leftEncoder = new Encoder(4, 5);
+  private final Encoder rightEncoder = new Encoder(6, 7);
+
+  // Set up the differential drive controller
+  private final DifferentialDrive diffDrive =
+      new DifferentialDrive(leftMotor::set, rightMotor::set);
+
+  // Set up the RomiGyro
+  private final RomiGyro gyro = new RomiGyro();
+
+  // Set up the BuiltInAccelerometer
+  private final BuiltInAccelerometer accelerometer = new BuiltInAccelerometer();
+
+  // -----------------------------------------------------------
+  // Initialization
+  // -----------------------------------------------------------
+
+  /** Creates a new Drivetrain. */
+  public Drivetrain() {
+    SendableRegistry.addChild(this.diffDrive, this.leftMotor);
+    SendableRegistry.addChild(this.diffDrive, this.rightMotor);
+
+    // We need to invert one side of the drivetrain so that positive voltages
+    // result in both sides moving forward. Depending on how your robot's
+    // gearbox is constructed, you might have to invert the left side instead.
+    this.rightMotor.setInverted(true);
+
+    // Use inches as unit for encoder distances
+    this.leftEncoder.setDistancePerPulse((Math.PI * Constants.kWheelDiameterMeters) / Constants.kCountsPerRevolution);
+    this.rightEncoder.setDistancePerPulse((Math.PI * Constants.kWheelDiameterMeters) / Constants.kCountsPerRevolution);
+    resetEncoders();
+  }
+
+  // -----------------------------------------------------------
+  // Control Output
+  // -----------------------------------------------------------
+
+  public void arcadeDrive(double xaxisSpeed, double zaxisRotate) {
+    this.diffDrive.arcadeDrive(xaxisSpeed, zaxisRotate);
+  }
+
+  public void resetEncoders() {
+    this.leftEncoder.reset();
+    this.rightEncoder.reset();
+  }
+
+  public void zeroHeading() {
+    this.gyro.reset();
+  }
+
+  /** Reset the gyro. */
+  public void resetGyro() {
+    this.gyro.reset();
+  }
+
+  // -----------------------------------------------------------
+  // System State
+  // -----------------------------------------------------------
+  public int getLeftEncoderCount() {
+    return this.leftEncoder.get();
+  }
+
+  public int getRightEncoderCount() {
+    return this.rightEncoder.get();
+  }
+
+  public double getLeftDistanceMeters() {
+    return this.leftEncoder.getDistance();
+  }
+
+  public double getRightDistanceMeters() {
+    return this.rightEncoder.getDistance();
+  }
+
+  public double getAverageDistanceInch() {
+    return (getLeftDistanceMeters() + getRightDistanceMeters()) / 2.0;
+  }
+
+  /**
+   * The acceleration in the X-axis.
+   *
+   * @return The acceleration of the Romi along the X-axis in Gs
+   */
+  public double getAccelX() {
+    return this.accelerometer.getX();
+  }
+
+  /**
+   * The acceleration in the Y-axis.
+   *
+   * @return The acceleration of the Romi along the Y-axis in Gs
+   */
+  public double getAccelY() {
+    return this.accelerometer.getY();
+  }
+
+  /**
+   * The acceleration in the Z-axis.
+   *
+   * @return The acceleration of the Romi along the Z-axis in Gs
+   */
+  public double getAccelZ() {
+    return this.accelerometer.getZ();
+  }
+
+  /**
+   * Current angle of the Romi around the X-axis.
+   *
+   * @return The current angle of the Romi in degrees
+   */
+  public double getGyroAngleX() {
+    return this.gyro.getAngleX();
+  }
+
+  /**
+   * Current angle of the Romi around the Y-axis.
+   *
+   * @return The current angle of the Romi in degrees
+   */
+  public double getGyroAngleY() {
+    return this.gyro.getAngleY();
+  }
+
+  /**
+   * Current angle of the Romi around the Z-axis.
+   *
+   * @return The current angle of the Romi in degrees
+   */
+  public double getGyroAngleZ() {
+    return this.gyro.getAngleZ();
+  }
+
+  public Rotation2d getHeading() {
+    return new Rotation2d(getGyroAngleZ() * (Math.PI/180));
+  }
+
+  // -----------------------------------------------------------
+  // Process Logic
+  // -----------------------------------------------------------
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
